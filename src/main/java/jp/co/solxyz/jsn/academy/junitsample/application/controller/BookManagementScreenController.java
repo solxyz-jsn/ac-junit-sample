59d821cd9a9d63fb7376c59f80a9d866d3779ec0
rykomatsu <rykomatsu@solxyz.co.jp>
2021-04-22 17:48:39 +0900

    テストクラス追加

--- src/main/java/jp/co/solxyz/jsn/academy/junitsample/application/controller/BookManagementScreenController.java
+++ src/main/java/jp/co/solxyz/jsn/academy/junitsample/application/controller/BookManagementScreenController.java
@@ -24,71 +24,71 @@ import lombok.AllArgsConstructor;
 public class BookManagementScreenController {
 
 	/** 書籍管理画面サービス */
-    @Autowired
-    private BookManagementService bookManagementService;
+	@Autowired
+	private BookManagementService bookManagementService;
 
-    /**
-     * Form初期処理
-     * @return Form
-     */
-    @ModelAttribute
-    BookManagementScreenForm setUpForm() {
-        return new BookManagementScreenForm();
-    }
-    
-    /**
-     * 初期表示
-     * @return 画面
-     */
-    @GetMapping
-    public ModelAndView init() {
-    	ModelAndView view = new ModelAndView("bookmanager");
-    	
-    	List<BookManagementTableDto> result = bookManagementService.init();
-    	
-    	if (result != null) {
-    		view.addObject("books", result);
-    	} else {
-    		view.addObject("message", "書籍情報がありません。");
-    	}
-    	
-        return view;
-    }
+	/**
+	 * Form初期処理
+	 * @return Form
+	 */
+	@ModelAttribute
+	BookManagementScreenForm setUpForm() {
+		return new BookManagementScreenForm();
+	}
 
-    /**
-     * 書籍変更処理
-     * @return 画面
-     */
-    @PostMapping(params="update")
-    public ModelAndView updateBookInfo(BookManagementScreenForm form) {
-    	ModelAndView view = new ModelAndView("bookmanager");
-    	int status = bookManagementService.update(form.getBookId(), form.getBookName(), form.getStock());
-    	
-    	if (status == 0) {
-    		view.addObject("message", "正常に更新されました。");
-    	} else {
-    		view.addObject("message", "更新に失敗しました。");
-    	}
-    	
-        return view;
-    }
+	/**
+	 * 初期表示
+	 * @return 画面
+	 */
+	@GetMapping
+	public ModelAndView init() {
+		ModelAndView view = new ModelAndView("bookmanager");
 
-    /**
-     * 書籍発注処理
-     * @return 画面
-     */
-    @PostMapping(params="order")
-    public ModelAndView orderBook(BookManagementScreenForm form) {
-    	ModelAndView view = new ModelAndView("bookmanager");
-    	int status = bookManagementService.order(form.getBookId(), form.getBookName(), form.getStock());
-    	
-    	if (status == 0) {
-    		view.addObject("message", "正常に発注されました。");
-    	} else {
-    		view.addObject("message", "発注に失敗しました。");
-    	}
-    	
-        return view;
-    }
+		List<BookManagementTableDto> result = bookManagementService.init();
+
+		if (result != null) {
+			view.addObject("books", result);
+		} else {
+			view.addObject("message", "書籍情報がありません。");
+		}
+
+		return view;
+	}
+
+	/**
+	 * 書籍変更処理
+	 * @return 画面
+	 */
+	@PostMapping(params = "update")
+	public ModelAndView updateBookInfo(BookManagementScreenForm form) {
+		ModelAndView view = new ModelAndView("bookmanager");
+		int status = bookManagementService.update(form.getBookId(), form.getBookName(), form.getStock());
+
+		if (status == 0) {
+			view.addObject("message", "正常に更新されました。");
+		} else {
+			view.addObject("message", "更新に失敗しました。");
+		}
+
+		return view;
+	}
+
+	/**
+	 * 書籍発注処理
+	 * @return 画面
+	 */
+	@PostMapping(params = "order")
+	public ModelAndView orderBook(BookManagementScreenForm form) {
+		ModelAndView view = new ModelAndView("bookmanager");
+		int status = bookManagementService.order(form.getBookId(), form.getBookName(), form.getStock());
+
+		if (status == 0) {
+			view.addObject("message", "正常に発注されました。");
+		} else {
+			view.addObject("message", "発注に失敗しました。");
+		}
+
+		return view;
+	}
 
 }
---
+++ src/test/java/jp/co/solxyz/jsn/academy/junitsample/application/controller/BookManagementScreenControllerTest.java
@@ -0,0 +1,25 @@
+package jp.co.solxyz.jsn.academy.junitsample.application.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+class BookManagementScreenControllerTest {
+
+	@InjectMocks
+	BookManagementScreenController sut;
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+}
